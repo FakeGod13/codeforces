a435c5cdce5df67c617844782615dddfa669753d
summeriver <83163060+FakeGod13@users.noreply.github.com>
2022-09-09 16:12:04 +0800

    更新

--- 785/A.cpp
+++ 785/A.cpp
@@ -9,18 +9,18 @@ int main()
 
 	while( t-- ){
 		string s; cin >> s;
-		
+
 		int A = 0,B = 0;
 		int l = s.size();
-		
+
 		if( l%2 ){
 			if(s[0]>s[l-1]) {
-				for(int i=0;i<l-1;i++){ 
+				for(int i=0;i<l-1;i++){
 					A += s[i]-'a'+1;
 				}
 				B += s[l-1]-'a'+1;
 			}else{
-				for(int i=1;i<l;i++) 
+				for(int i=1;i<l;i++)
 					A += s[i]-'a'+1;
 				B += s[0]-'a'+1;
 			}
